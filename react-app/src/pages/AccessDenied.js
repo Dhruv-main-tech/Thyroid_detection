9193d4b6eebcd3b2cfc334c3af2719547fdb9b6d
Dhruv69408012 <dhruv408012@gmail.com>
2024-01-21 23:20:29 +0530

    Final

--- react-app/src/pages/AccessDenied.js
+++ react-app/src/pages/AccessDenied.js
@@ -1,7 +1,13 @@
 const AccessDenied = () => {
   return (
-    <div>
-      <div>AccessDenied</div>
+    <div className="home" style={{ padding: "50px" }}>
+      <div className="form_container2" style={{ maxHeight: "80px" }}>
+        <div className="form profile_form">
+          <div className="profile_form">
+            <div style={{color:"red"}}>ACCESS DENIED</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
