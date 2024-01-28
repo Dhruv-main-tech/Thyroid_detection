e0b81baa8c5da439b2ff118666f00628cab6e298
Dhruv69408012 <dhruv408012@gmail.com>
2024-01-28 22:09:21 +0530

    Final

--- Backend/connectDB.js
+++ Backend/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI1);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connect to user db success");
   } catch (error) {
     console.log(error);
